6871fe780e6b6de061662ba94d21e52d57ed1223
Stefan Mastilak <stefan.mastilak@visma.com>
2022-08-03 13:28:53 +0200

    task3

---
+++ python_solutions/hashtag_generator.py
@@ -0,0 +1,60 @@
+# REF: stefan.mastilak@gmail.com
+
+"""
+
+Task definition:
+-----------------
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+Here's the deal:
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+
+Examples:
+---------
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+
+"""
+
+import unittest
+
+
+def hashtag_generator(phrase: str):
+    """
+    Function for generating hashtag based on input phrase
+    :param phrase: input text to be converted to hashtag
+    :return: Hashtag word
+    :rtype: str
+    """
+    result = "#"
+    for i in phrase.split():
+        result += i.capitalize()
+    return result if phrase else False
+
+
+class TestSolution(unittest.TestCase):
+    """
+    Test solution
+    """
+    def test1(self):
+        test_phrase = " Hello there thanks for trying my Kata"
+        result = hashtag_generator(phrase=test_phrase)
+        self.assertEqual(result, "#HelloThereThanksForTryingMyKata")
+
+    def test2(self):
+        test_phrase = "    Hello     World   "
+        result = hashtag_generator(phrase=test_phrase)
+        self.assertEqual(result, "#HelloWorld")
+
+    def test3(self):
+        test_phrase = ""
+        result = hashtag_generator(phrase=test_phrase)
+        self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
