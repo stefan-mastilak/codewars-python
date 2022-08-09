b7547dc859de2acb73d61fb96aaae626562ea7b7
Stefan Mastilak <stefan.mastilak@visma.com>
2022-08-09 10:27:32 +0200

    refs

--- python_solutions/alphabet_position.py
+++ python_solutions/alphabet_position.py
@@ -26,21 +26,22 @@ def alphabet_position(text: str):
     :return: alphabet position of the letters in text
     :rtype: str
     """
-    result = str()
-    for i in text:
-        if i.isalpha():
-            result += f" {str(ascii_lowercase.index(i.lower()) + 1)}"
-    return result.strip()
+    return ' '.join(str(ascii_lowercase.index(i.lower()) + 1) for i in text if i.isalpha())
 
 
 class TestSolution(unittest.TestCase):
+    """
+    Test solution
+    """
     def test1(self):
-        """
-        Test solution
-        """
         test_string = "The sunset sets at twelve o' clock."
         result = alphabet_position(text=test_string)
         self.assertEqual(result, "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
+
+    def test2(self):
+        test_string = "'012 * bb*-"
+        result = alphabet_position(text=test_string)
+        self.assertEqual(result, "2 2")
 
 
 if __name__ == "__main__":
