eae352b4edeacbad4c1b4668d12aa73a9d8fb040
Stefan Mastilak <stefan.mastilak@visma.com>
2022-09-06 11:06:17 +0200

    adding_tests

--- python_solutions/human_time.py
+++ python_solutions/human_time.py
@@ -32,7 +32,7 @@ def human_time_converter(seconds: int):
     :return: human-readable time as string
     :rtype: str
     """
-    return f"{(seconds//3600):02d}:{(seconds // 60 % 60):02d}:{(seconds % 60):02d}" if seconds <= 359999 else False
+    return f"{(seconds//3600):02d}:{(seconds // 60 % 60):02d}:{(seconds % 60):02d}" if 0 <= seconds <= 359999 else False
 
 
 class TestSolution(unittest.TestCase):
@@ -53,6 +53,16 @@ class TestSolution(unittest.TestCase):
         test_input = 360000
         result = human_time_converter(seconds=test_input)
         self.assertFalse(result)
+
+    def test4(self):
+        test_input = -360
+        result = human_time_converter(seconds=test_input)
+        self.assertFalse(result)
+
+    def test5(self):
+        test_input = 0
+        result = human_time_converter(seconds=test_input)
+        self.assertTrue(result, "00:00:00")
 
 
 if __name__ == "__main__":
